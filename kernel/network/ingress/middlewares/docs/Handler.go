daf5137754b8127ac5f848f2a0e531917e2deda8
Christophe Tiraoui <krux@krux.fr>
2025-04-19 17:54:57 +0200

    fix doc relativity

--- kernel/network/ingress/middlewares/docs/Handler.go
+++ kernel/network/ingress/middlewares/docs/Handler.go
@@ -2,12 +2,14 @@ package docs
 
 import (
 	_ "embed"
+	"net/url"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/swag"
+	"github.com/tuxounet/k2-sdk/kernel/config"
 	"github.com/tuxounet/k2-sdk/types"
 )
 
@@ -26,7 +28,21 @@ func Register(service types.IKernelService, docs *swag.Spec, router *gin.RouterG
 		ginSwagger.URL(baseRoute+"openapi.json")),
 	)
 
+	configService := service.GetKernel().GetService(config.ServiceKey).(*config.Service)
+	rootUrl, err := configService.GetAsString("host.ingress.rootUrl")
+	if err != nil {
+		log.ErrorF("Error getting rootUrl: %v", err)
+		return err
+	}
+	paredUrl, err := url.Parse(rootUrl)
+	if err != nil {
+		log.ErrorF("Error parsing rootUrl: %v", err)
+		return err
+	}
+
 	router.GET("/openapi.json", func(c *gin.Context) {
+		docs.Schemes = []string{paredUrl.Scheme}
+		docs.Host = paredUrl.Host
 		docs.BasePath = baseRoute
 		swagger := swag.GetSwagger(docs.InfoInstanceName)
 		if swagger == nil {
